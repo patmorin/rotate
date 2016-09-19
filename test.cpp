f2735a44fcd4ebc0d3025bae5a1299737e4d8d40
Pat Morin <morin@scs.carleton.ca>
2016-09-19 11:49:06 -0400

    Some cleanups and one big speedup

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include<chrono>
 #include<iostream>
+#include<iomanip>
 #include<sstream>
 #include<numeric>
 #include<algorithm>
@@ -49,19 +50,18 @@ I gcd(I a, I b) {
 template<typename T, typename I>
 void rotate_gcd(T *a, I n, I r) {
     I g = gcd(n, r);
-    //std::cout << "gcd(" << n << "," << r << ")=" << g << std::endl;
     I m = n / g;
     for (I k = 0; k < g; k++) {
         I i = k;
         T t = a[i];
         for (I j = 0; j < m; j++) {
-            I inxt = (i+r)%n;
+            I inxt = (i+r);
+						inxt = inxt >= n ? inxt-n : inxt; // avoid mod in favour of cmov
             T t2 = a[inxt];
-            //std::cout << t << "->" << a[inxt] << std::endl;
             a[inxt] = t;
             t = t2;
             i = inxt;
-        } 
+        }
     }
 }
 
@@ -85,10 +85,13 @@ void speed_test(T *a, I n, I r, void(*rot)(T*,I,I)) {
 }
 
 typedef std::uint64_t Index;
+typedef std::uint32_t Data;
 
+typedef void(*Rotater)(Data*,Index,Index);
 
 int main(int argc, char *argv[]) {
     Index n, r;
+
     if (argc != 3) usage(argv[0]);
     std::istringstream is_n(argv[1]);
     is_n >> n;
@@ -96,47 +99,30 @@ int main(int argc, char *argv[]) {
     is_r >> r;
 
     std::cout << "n = " << n << ", r = " << r << std::endl;
-	
-	auto *a = new std::uint32_t[n];
 
-    std::iota(a, a+n, 1);
-    print_array(a, n);
-    rotate_std(a, n, r);
-    print_array(a, n);
+		auto *a = new Data[n];
 
-    std::cout << std::endl;
+		Rotater fns[] =
+		{ rotate_std<Data,Index>, rotate_stdrev<Data,Index>,
+			rotate_rev<Data,Index>, rotate_gcd<Data,Index> };
+		std::string fn_names[] =
+		{ "rotate_std", "rotate_stdrev", "rotate_rev", "rotate_gcd" };
 
-    std::iota(a, a+n, 1);
-    print_array(a, n);
-    rotate_stdrev(a, n, r);
-    print_array(a, n);
+		if (n <= 50) {
+			for (int i = 0; i < 4; i++) {
+				std::iota(a, a+n, 1);
+				print_array(a, n);
+				rotate_std(a, n, r);
+				print_array(a, n);
+				std::cout << std::endl;
+			}
+		} else {
+			for (int i = 0; i < 4; i++) {
+		    std::iota(a, a+n, 1);
 
-    std::cout << std::endl;
-
-
-    std::iota(a, a+n, 1);
-    print_array(a, n);
-    rotate_rev(a, n, r);
-    print_array(a, n);
-
-    std::cout << std::endl;
-
-    std::iota(a, a+n, 1);
-    print_array(a, n);
-    rotate_gcd(a, n, r);
-    print_array(a, n);
-
-    std::iota(a, a+n, 1);
-    speed_test(a, n, r, rotate_std); 
-
-    std::iota(a, a+n, 1);
-    speed_test(a, n, r, rotate_stdrev); 
-
-    std::iota(a, a+n, 1);
-    speed_test(a, n, r, rotate_rev); 
-
-    std::iota(a, a+n, 1);
-    speed_test(a, n, r, rotate_gcd); 
-    
+				std::cout << std::setw(20) << fn_names[i] << " : ";
+		    speed_test(a, n, r, fns[i]);
+			}
+		}
     return 0;
 }
