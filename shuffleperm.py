17d8100aa458cc524fde9ee0e99f62b2b2a1bc52
Pat Morin <morin@scs.carleton.ca>
2016-09-20 10:08:03 -0400

    Cleanup

--- shuffleperm.py
+++ shuffleperm.py
@@ -8,13 +8,27 @@ def f(i, n):
         return i//2
 
 def print_cycles(cycles):
-    print("(" + ")*(".join([",".join([str(x) for x in c]) for c in cycles]) +")")
+    print("(" + ")*(".join([",".join([str(x) for x in c])
+                                      for c in cycles]) +")")
 
 def print_bin_cycles(cycles):
-    print("(" + ")*(".join([",".join(["{0:05b}".format(x) for x in c]) for c in cycles]) +")")
+    print("(" + ")*(".join([",".join(["{0:05b}".format(x) for x in c])
+                                      for c in cycles]) +")")
+
+def read_cmd_line():
+    n = -1
+    if len(sys.argv) == 2:
+        try:
+            n = int(sys.argv[1])
+        except ValueError:
+            sys.stderr.write("Unable to parse argument '{}'\n".format(sys.argv[1]))
+    if n <= 0:
+        sys.stderr.write("Usage: {} <n>\n".format(sys.argv[0]))
+        sys.exit(-1)
+    return n
 
 if __name__ == "__main__":
-    n = int(sys.argv[1])
+    n = read_cmd_line()
     marked = set()
     cycles = []
     for i in range(n):
