66b96b277b5a258f22530ed921193c1dbe3b490b
Pat Morin <morin@scs.carleton.ca>
2016-09-21 15:58:47 -0400

    Added Added other shuffling options

--- shuffleperm.py
+++ shuffleperm.py
@@ -1,11 +1,15 @@
 from __future__ import print_function
 import sys
 
-def f(i, n):
+def my_outshuffle(i, n):
+    """This is the outshuffle permutation we need for Eytzinger layouts"""
     if i % 2 == 0:
         return n//2+i//2
     else:
         return i//2
+
+def std_inshuffle(i, n):
+    return (2*(i+1) % (n+1))-1
 
 def print_cycles(cycles):
     print("(" + ")*(".join([",".join([str(x) for x in c])
@@ -14,6 +18,23 @@ def print_cycles(cycles):
 def print_bin_cycles(cycles):
     print("(" + ")*(".join([",".join(["{0:05b}".format(x) for x in c])
                                       for c in cycles]) +")")
+
+def get_cycles(f, n):
+    """Get the cycles of the permutation f(0),...,f(n-1)"""
+    marked = set()
+    cycles = []
+    for i in range(n):
+        if i not in marked:
+            marked.add(i)
+            c = [i]
+            j = f(i, n)
+            while j != i:
+                c.append(j)
+                marked.add(j)
+                j = f(j, n)
+            cycles.append(c)
+    return cycles
+
 
 def read_cmd_line():
     n = -1
@@ -30,17 +51,9 @@ def read_cmd_line():
 if __name__ == "__main__":
     n = read_cmd_line()
     marked = set()
-    cycles = []
-    for i in range(n):
-        if i not in marked:
-          marked.add(i)
-          c = [i]
-          j = f(i, n)
-          while j != i:
-              c.append(j)
-              marked.add(j)
-              j = f(j, n)
-          cycles.append(c)
-
+    cycles = get_cycles(std_inshuffle, n)
     print_cycles(cycles)
-    print_bin_cycles(cycles)
+    # print_bin_cycles(cycles)
+    cycles = get_cycles(my_outshuffle, n)
+    print_cycles(cycles)
+    # print_bin_cycles(cycles)
